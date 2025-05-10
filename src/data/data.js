2fb6816d784939d42b7585a5f778b69cf9cfcbfc
Yosefinagst <yosefnaagustine@gmail.com>
2025-05-10 07:32:05 +0700

    Update Oprec Staff Hology

--- src/data/data.js
+++ src/data/data.js
@@ -2372,7 +2372,7 @@ Catatan:
     ],
     logo: LOGO_BEM, // gambar logo di home, login, dan robot image
     landingImage: `https://res.cloudinary.com/dks74omr4/image/upload/v1746832625/js19zbjxdrjjns2zwtrs.jpg`,
-    openDate: new Date("2025-04-17T19:00:00.999+07:00"),
+    openDate: new Date("2025-05-17T19:00:00.999+07:00"),
     closeDate: new Date("2025-05-21T23:59:00.999+07:00"),
     announceDate: new Date("2025-05-26T19:00:00.999+07:00"),
     onlyFor: [23, 24], // Angkatan berapa yg diterima
